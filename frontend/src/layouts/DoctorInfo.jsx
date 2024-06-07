4ab8338dfe66537944a19551d945edf6ae1d0696
allenfbarry <srisahith002@gmail.com>
2024-06-07 16:28:49 -0600

    added initial project files

---
+++ frontend/src/layouts/DoctorInfo.jsx
@@ -0,0 +1,166 @@
+import { useState } from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { addTocart } from "../slices/cartSlice";
+import { useGetStaffByIDQuery } from "../slices/doctorsApiSlice";
+function DoctorInfo() {
+  const { userInfo } = useSelector((state) => state.auth);
+  const [selectedSlot, setSelectedSlot] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { id: doctorId } = useParams();
+  const { data: doctor, isLoading, error } = useGetStaffByIDQuery(doctorId);
+  const dates = JSON.parse(localStorage.getItem("dates"));
+  const apponitmentDate = new Date(dates).toLocaleDateString("en-US");
+
+  const check2 = () => {
+    const t = userInfo.appointments.map((app) =>
+      app.atype === doctor?.specialization?.name &&
+      apponitmentDate === new Date(app.date).toLocaleDateString("en-US")
+        ? app.slot
+        : false
+    );
+
+    const fil = t.filter((t) => t !== false);
+    return fil;
+  };
+
+  const dup = check2();
+  console.log(dup);
+
+  const check = (slot) => {
+    const t = userInfo.appointments.filter(
+      (app) =>
+        app.slot === slot.slot &&
+        apponitmentDate === new Date(app.date).toLocaleDateString("en-US")
+    );
+    console.log(t);
+    return t[0];
+  };
+
+  const isAvailable = (slot) => {
+    const isFound = slot.unavailableDates.some((date) =>
+      apponitmentDate.includes(new Date(date).toLocaleDateString("en-US"))
+    );
+
+    return !isFound;
+  };
+
+  const handleSelect = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setSelectedSlot(
+      checked
+        ? [...selectedSlot, value]
+        : selectedSlot.filter((item) => item !== value)
+    );
+  };
+
+  const handleClick = () => {
+    const slot = doctor.schedule.filter(
+      (slot) => slot._id === selectedSlot?.join(" ")
+    );
+
+    dispatch(
+      addTocart({
+        firstName: doctor.firstName,
+        lastName: doctor.lastName,
+        doctor: doctor._id,
+        specialization: doctor.specialization.name,
+        slot_id: selectedSlot[0],
+        apponitmentDate,
+        dates,
+        slot: slot[0].slot,
+      })
+    );
+    navigate("/confirm");
+  };
+  return (
+    <div>
+      <LinkContainer to="/doctors">
+        <Button variant="dark">Back</Button>
+      </LinkContainer>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error?.data?.message || error.error}</p>
+      ) : (
+        <>
+          <Row>
+            <h4 className="text-center my-4">
+              Appointment Date: {apponitmentDate}
+            </h4>
+            <Col md={{ span: 6, offset: 3 }}>
+              <Card className="p-2">
+                <Card.Title className="p-3">
+                  <h1>
+                    Dr. {doctor.firstName} {doctor.lastName}
+                  </h1>
+                </Card.Title>
+                <Card.Body>
+                  <Card.Text>
+                    <h2>Specialization: {doctor.specialization.name}</h2>
+                  </Card.Text>
+                </Card.Body>
+
+                {dup.length !== 0 ? (
+                  `You Already Have Appointment with ${doctor.specialization.name} on ${apponitmentDate}`
+                ) : (
+                  <>
+                    <Card.Body className="my-3">
+                      <Col className="py-3">
+                        <strong>Select Time Slot For Appointment:</strong>
+                      </Col>
+                      <Col className="py-3">
+                        <div className="rSelectRooms">
+                          {doctor.schedule.map((slot) => (
+                            <div className="room" key={slot.slot}>
+                              <input
+                                style={{ width: "20px", height: "20px" }}
+                                type="checkbox"
+                                value={slot._id}
+                                onChange={handleSelect}
+                                disabled={!isAvailable(slot) || check(slot)}
+                              />
+                              <label>{slot.slot} </label>
+                              <p>
+                                <strong>
+                                  {" "}
+                                  {/* {isAvailable(slot)
+                                ? "(Available)"
+                                : "(Not Available)"} */}
+                                  {check(slot) ? (
+                                    <p>
+                                      "You already have another appointment during
+                                      this time"
+                                    </p>
+                                  ) : isAvailable(slot) ? (
+                                    "(Available)"
+                                  ) : (
+                                    "(Not Available)"
+                                  )}
+                                </strong>
+                              </p>
+                            </div>
+                          ))}
+                        </div>
+                      </Col>
+                    </Card.Body>
+                    <Card.Body>
+                      <Button variant="dark" onClick={handleClick}>
+                        Confirm
+                      </Button>
+                    </Card.Body>
+                  </>
+                )}
+              </Card>
+            </Col>
+          </Row>
+        </>
+      )}
+    </div>
+  );
+}
+export default DoctorInfo;
